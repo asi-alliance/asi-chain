4c8ef050a2e7ed0a853575322f810a0eb3a2cd25
Andrew Serbin <serbinadrian@outlook.com>
2025-08-14 13:07:51 +0300

    Fixed faucet API JSON body

--- faucet/src/api/index.ts
+++ faucet/src/api/index.ts
@@ -2,7 +2,7 @@ import { BASE_URL } from "@utils/config";
 
 export interface IStatusResponse {
     status: string;
-    msg?: string
+    msg?: string;
 }
 
 export const getBalance = async (walletAddress: string): Promise<number> => {
@@ -27,7 +27,10 @@ export const faucetTokens = async (walletAddress: string): Promise<string> => {
         method: "POST",
         mode: "cors",
         credentials: "omit",
-        headers: { Accept: "application/json" },
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
         body: JSON.stringify({ to_address: walletAddress }),
     });
 
