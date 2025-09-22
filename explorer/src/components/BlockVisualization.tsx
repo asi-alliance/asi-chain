1732828f4c1b17905180fa74e06c662cadfb3ee7
Semyon Novikov <semyon.novikov@singularitynet.io>
2025-09-22 18:33:50 +0300

    refactor BlockVisualization.tsx

--- explorer/src/components/BlockVisualization.tsx
+++ explorer/src/components/BlockVisualization.tsx
@@ -356,7 +356,8 @@ const BlockVisualization: React.FC<BlockVisualizationProps> = ({
                                 cy="50%"
                                 outerRadius={100}
                                 dataKey="blockCount"
-                                label={(p: any) => `${(p.percent ? p.percent * 100 : p.payload?.percentage ?? 0).toFixed(1)}%`}
+                                label={(p: any) => `${(p?.payload?.percentage ?? 0).toFixed(1)}%`}
+                                // label={(p: any) => {console.log("P ", p); return `${(p.percent ? p.percent * 100 : p.payload?.percentage ?? 0).toFixed(1)}%`}}
                             >
                                 {proposerData.map((entry, index) => (
                                     <Cell key={`cell-${index}`} fill={entry.color} />
